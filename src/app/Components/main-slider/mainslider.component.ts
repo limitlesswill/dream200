54b83d7342a15bc0d4343f1066a2b9e7030dd546
limitlesswill <wa.elose@gmail.com>
2024-04-29 15:46:57 +0300

    partial localization.. wait for part two

--- src/app/Components/main-slider/mainslider.component.ts
+++ src/app/Components/main-slider/mainslider.component.ts
@@ -1,11 +1,24 @@
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'mainslider',
   standalone: true,
+  imports:[TranslateModule],
   templateUrl: './mainslider.component.html',
   styleUrl: './mainslider.component.css'
 })
 export class MainsliderComponent {
 
+  lang:any; 
+  langChangeSubscription: Subscription;
+  
+  constructor(private translate: TranslateService) {
+    this.lang = localStorage.getItem('lang')
+    translate.use(this.lang);
+    this.langChangeSubscription = translate.onLangChange.subscribe(event => {
+      this.lang = event.lang;
+    });
+  }
 }
--- src/app/Components/static-items/static-items/item1/item1.component.ts
+++ src/app/Components/static-items/static-items/item1/item1.component.ts
@@ -1,13 +1,26 @@
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'item1',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TranslateModule],
   templateUrl: './item1.component.html',
   styleUrl: './item1.component.css'
 })
 export class Item1Component {
+
+  lang:any; 
+  langChangeSubscription: Subscription;
+  
+  constructor(private translate: TranslateService) {
+    this.lang = localStorage.getItem('lang')
+    translate.use(this.lang);
+    this.langChangeSubscription = translate.onLangChange.subscribe(event => {
+      this.lang = event.lang;
+    });
+  }
 
 }
